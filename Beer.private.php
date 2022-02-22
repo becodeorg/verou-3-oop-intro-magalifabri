112c5dcb541efcbf823b5f2068020faaa4020393
Magali Fabri <magali_90@live.nl>
2022-02-22 13:30:08 +0100

    Add type declarations and implement getters and setters

--- Beer.private.php
+++ Beer.private.php
@@ -1,35 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 require_once './Beverage.private.php';
 
 class Beer_private extends Beverage_private
 {
-    private $name;
-    private $alcoholPercentage;
+    private string $name;
+    private float $alcoholPercentage;
 
-    public function __construct(string $name, string $color, float $price, float $alcoholPercentage, string $temperature = "cold")
-    {
+    public function __construct(
+        string $name,
+        string $color,
+        float $price,
+        float $alcoholPercentage,
+        string $temperature = "cold"
+    ) {
         $this->name = $name;
-        $this->color = $color;
-        $this->price = $price;
-        $this->temperature = $temperature;
+        $this->setColor($color);
+        $this->setPrice($price);
+        $this->setTemperature($temperature);
         $this->alcoholPercentage = $alcoholPercentage;
     }
 
-    private function beerInfo()
+    private function beerInfo(): string
     {
-        return "Hi i'm $this->name and have an alcohol percentage of $this->alcoholPercentage and I have a $this->color color.";
+        return "Hi i'm $this->name and have an alcohol percentage of $this->alcoholPercentage and I have a {$this->getColor()} color.";
     }
 
-    public function getAlcoholpercentage()
+    public function getAlcoholpercentage(): float
     {
         return $this->alcoholPercentage;
     }
-    public function getColor()
-    {
-        return $this->color;
-    }
-    public function getBeerInfo()
+    public function getBeerInfo(): string
     {
         return $this->beerInfo();
     }
--- Beverage.private.php
+++ Beverage.private.php
@@ -1,19 +1,50 @@
 <?php
 
+declare(strict_types=1);
+
 class Beverage_private
 {
-    private $color;
-    private $price;
-    private $temperature;
+    private string $color;
+    private float $price;
+    private string $temperature;
 
-    public function __construct($color, $price, $temperature = "cold")
-    {
+    public function __construct(
+        string $color,
+        float $price,
+        string $temperature = "cold"
+    ) {
         $this->color = $color;
         $this->price = $price;
         $this->temperature = $temperature;
     }
 
-    public function getInfo()
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+    public function getTemperature(): string
+    {
+        return $this->temperature;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+    public function setTemperature($temperature)
+    {
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo(): string
     {
         return "This beverage is $this->temperature and $this->color.";
     }
